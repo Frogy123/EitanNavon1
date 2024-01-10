0b86459caf0e28411c5af743eba823a06d1131c6
Frogy <thefrogeitan@gmail.com>
2024-01-10 15:11:21 +0200

    final

--- app/src/main/java/com/example/eitannavon1/MainActivity.java
+++ app/src/main/java/com/example/eitannavon1/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.eitannavon1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn1.setTextColor(Color.GREEN);
+                btn1.setText("ohh ive been pressed");
+            }
+        });
     }
 }
